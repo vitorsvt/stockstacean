dcad407ba2188509753f72ee6e9c2d78a751da3e
Vitor Silvestri <vitorsilvestri1@gmail.com>
2021-03-17 21:14:33 -0300

    add documentation to bitboard

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -1,15 +1,18 @@
+/// Each bit represents a square on the board
 #[derive(Debug)]
 pub struct Bitboard(pub u64);
 
 pub const EMPTY: Bitboard = Bitboard(0);
 
 impl Bitboard {
+    /// Set a bit on the bitboard
     pub fn set(&mut self, rank: u8, file: u8) {
         if rank < 8 && file < 8 {
             self.0 |= 1u64 << (rank * 8 + file);
         }
     }
 
+    /// Unset a bit on the bitboard
     pub fn unset(&mut self, rank: u8, file: u8) {
         if rank < 8 && file < 8 {
             self.0 ^= 1u64 << (rank * 8 + file);
